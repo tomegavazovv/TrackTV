fa1f072776645abc10912ef615e9e22ab39d6e0c
tomegavazovv <tomegavazov@gmail.com>
2023-07-28 12:45:25 +0200

    init

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './pages/home-page/search-bar/search-bar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RecentlyWatchedComponent } from './recently-watched/recently-watched.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    HomePageComponent,
+    RecentlyWatchedComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
